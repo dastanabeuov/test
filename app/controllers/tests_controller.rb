1dffa0a4b7a3c0d4d88ad855021b905e20f24f1b
Dastan Abeuov <d.abeuov9@gmail.com>
2018-10-31 14:58:56 +0600

    1. Реализуйте все стандартные действия вместе с представлениями (видами) для ресурса вопросов:

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -2,11 +2,9 @@ class TestsController < ApplicationController
 
   before_action :set_test, only: [:show]
 
-  def index
-    @tests = Test.all
-  end
-
   def show; end
+
+  private
 
   def set_test
     @test = Test.find(params[:id])
