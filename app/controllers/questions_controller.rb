b5d33aa863308c1234f70afdfe621a084a377d92
Dastan Abeuov <d.abeuov9@gmail.com>
2018-11-02 18:41:36 +0600

    исправления PR от бардака

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,16 +1,16 @@
 class QuestionsController < ApplicationController
   before_action :set_test, only: [:index, :new, :create]
-  before_action :set_question, only: [:show, :delete, :edit]
+  before_action :set_question, only: [:show, :delete]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+
+  def index; end
 
   def show; end
 
   def edit; end
 
-  def new; 
-    @question = Question.new
-  end
+  def new; end
 
   def create
     @question = @test.questions.new(question_params)
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,2 @@
 module ApplicationHelper
-  def current_year
-    Time.current.year
-  end
-
-  def github_url(author, repo)
-    "https://github.com/#{ author }/#{ repo }"
-  end
 end
