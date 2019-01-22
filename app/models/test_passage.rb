72a8b8d8200a2943c897ed81acc517a62e048d51
Dastan Abeuov <d.abeuov9@gmail.com>
2019-01-22 17:07:04 +0600

    lesson9 4 commit

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -15,11 +15,8 @@ class TestPassage < ApplicationRecord
   end
   
   def accept!(answer_ids)
-	  if correct_answer?(answer_ids)
-	    self.correct_questions += 1
-	  end
-	  self.current_question = next_question
-	  save!
+	  self.correct_questions += 1 if correct_answer?(answer_ids)
+    save!
   end
 
   def completed?
@@ -37,26 +34,17 @@ class TestPassage < ApplicationRecord
   private
 
   def before_validation_set_next_question
-    self.current_question = next_question
-  end
-
-  def before_validation_set_first_question
-  	self.current_question = test.questions.first if test.present?
+    return self.current_question = test.questions.first if test.present? && current_question.nil?
+    self.current_question = test.questions.order(:id).where('id > ?', current_question.id).first
   end
 
   def correct_answer?(answer_ids)
-  	correct_answers_count = correct_answers.count
-    (correct_answers_count == correct_answers.where(id: answer_ids).count) &&
-    correct_answers_count == answer_ids.count
+  	return false unless answer_ids
+    correct_answers.ids.sort == answer_ids.map(&:to_i).sort
   end
 
   def correct_answers
     current_question.answers.correct
-  end
-
-  def next_question
-    return test.questions.first if current_question.nil?
-  	test.questions.order(:id).where('id > ?', current_question.id).first
   end
 
 end
