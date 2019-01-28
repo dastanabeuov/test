5c1de03395e7a8eb489cff1543e2170c0d77d618
Dastan Abeuov <d.abeuov9@gmail.com>
2019-01-28 18:49:26 +0600

    first commit lesson12

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   
+  before_action :set_locale
   before_action :authenticate_user!
 
   def after_sign_in_path_for(user)
@@ -8,6 +9,16 @@ class ApplicationController < ActionController::Base
     else
       tests_path
     end
+  end
+
+  def default_url_options
+    { lang: I18n.locale == I18n.default_locale ? nil : I18n.locale }
+  end  
+
+  private
+
+  def set_locale
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
   end  
 
 end
