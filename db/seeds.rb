038fa49bdb23c9c3c8c27ff42b5f03963b4a4787
Dastan Abeuov <d.abeuov9@gmail.com>
2019-02-06 12:45:12 +0600

    finish edit gem file

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user = User.create!(
+  [
+    { email: 'd.abeuov9@gmail.com', password: 'A-z123123', 
+      password_confirmation: 'A-z123123', first_name: 'Dastan', 
+      last_name: 'Abeuov', type: 'Admin' },
+    { email: 'example@example.org', password: 'A-z123123', password_confirmation: 'A-z123123' }
+    ]
+  )
 
 categories = Category.create!(
   [
